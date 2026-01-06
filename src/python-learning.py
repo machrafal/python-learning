75a6cbfd37d24122b720f10bfd4b52d2631777ac
machrafal <mach.rafal@gmail.com>
2026-01-06 13:34:58 +0100

    second-commit

--- src/python-learning.py
+++ src/python-learning.py
@@ -1,3 +1,5 @@
+import calendar
+
 import numpy as np
 
 # 1. Write a Python program to print "Hello Python".
@@ -50,3 +52,69 @@ print(f"The swapped variables are: a = {a}, b = {b}")
 
 # 5. Write a Python program to generate a random number
 a = np.random.randint(1, 100)
+
+# 6. Write a Python program to convert kilometers to miles.
+kilometers = float(input("Enter distance in kilometers: "))
+# Conversion factor: 1 kilometer = 0.621371 miles
+conversion_factor = 0.621371
+miles = kilometers * conversion_factor
+print(f"The {kilometers} kilometers is {miles} miles.")
+
+# 7. Write a Python program to convert Celsius to Fahrenheit.
+celsius = float(input("Enter the temperature in celsius degrees: "))
+# Conversion formula: Fahrenheit = (Celsius * 9/5) + 32
+fahrenheit = celsius * 9 / 5 + 32
+print(f"The {celsius} temperature in Celsius is {fahrenheit} in Fahrenheit")
+
+# 8. Write a Python program to display calendar.
+year = int(input("Please enter the year: "))
+month = int(input("Please enter the month: "))
+cal = calendar.month(year, month)
+print(cal)
+
+# 9. Write a Python program to solve quadratic equation.
+# ax^2+bx+c=0
+a = float(input("Please enter a: "))
+b = float(input("Please enter b: "))
+c = float(input("Please enter c: "))
+
+delta = b**2 - 4 * a * c
+
+if delta > 0:
+    x1 = (-b + np.sqrt(delta)) / 2 * a
+    x2 = (-b - np.sqrt(delta)) / 2 * a
+    print(
+        f"The delta is greater than 0. The equation has 2 real roots.\nThe roots are {x1} and {x2}"
+    )
+elif delta == 0:
+    x0 = -b / 2 * a
+    print(f"The delta is equal to 0. The equation has 1 real root.\nThe root is {x0}")
+else:
+    real = -b / 2 * a
+    img = np.sqrt(abs(delta)) / 2 * a
+    print(
+        f"The delta is less than 0. The equation has complex roots.\nThe roots are {real} + {img}i and {real} - {img}i."
+    )
+
+# 10. Write a Python program to swap two variables without temp variable.
+a = float(input("Please enter the first variable: "))
+b = float(input("Please enter the second variable: "))
+print(f"The original variables are: a = {a} and b = {b}")
+a, b = b, a
+print(f"The swapped variables are: a = {a}, b = {b}")
+
+# 11. Write a Python Program to Check if a Number is Positive, Negative or Zero.
+a = float(input("Please enter your number: "))
+if a > 0:
+    print("The entered number is positive.")
+elif a == 0:
+    print("The entered number is 0.")
+else:
+    print("The entered number is negative.")
+
+# 12. Write a Python Program to Check if a Number is Odd or Even.
+a = float(input("Please enter a number: "))
+if a % 2 == 0:
+    print("The entered number is even.")
+else:
+    print("The entered number is odd.")
